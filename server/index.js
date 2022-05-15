789682e263bdcf2478a5046c94900f3226d7c56a
Aman <53884276+aman246149@users.noreply.github.com>
2022-05-15 19:10:04 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ var io=require("socket.io")(server)
 //middleware
 app.use(express.json());
 
-const Db="mongodb+srv://aman:9917387143@cluster0.6pfms.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
 
 
 //socket io 
@@ -78,11 +78,6 @@ app.get('/', function (req, res) {
   })
 
 
-mongoose.connect(Db).then(()=>{
-console.log("success")
-}).catch((e)=>{
-    console.log(e)
-})
 
 server.listen(port,"0.0.0.0",()=>{
     console.log("Server started and running on port " + port)
